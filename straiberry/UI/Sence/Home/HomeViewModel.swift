563a75e7606a3abc56de0cd287181c3397ad8ba5
behnam.hosseini <>
2022-06-20 21:03:14 +0200

    add view model details

--- straiberry/UI/Sence/Home/HomeViewModel.swift
+++ straiberry/UI/Sence/Home/HomeViewModel.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 class HomeViewModel{
-    
+    var homeViewModelDelegate:HomeViewModelDelegate?=nil
+    func getPhotoList(photoText:String){
+        NetworkManager.shared.call(type: .getPhotoList,params: ["text":photoText], callback: {
+            item in
+            do{
+            var photoList:PhotoList=try PhotoList(data: item)
+                self.homeViewModelDelegate?.onPhotoListLoad(photoList: photoList)
+            }catch {
+                self.homeViewModelDelegate?.onErrorHappend(errorMassage: "error parsing data")
+            }
+        }, fallback: {
+            item in
+            self.homeViewModelDelegate?.onErrorHappend(errorMassage: item)
+        })
+    }
+}
+protocol HomeViewModelDelegate{
+    func onPhotoListLoad(photoList:PhotoList)
+    func onErrorHappend(errorMassage:String)
 }
